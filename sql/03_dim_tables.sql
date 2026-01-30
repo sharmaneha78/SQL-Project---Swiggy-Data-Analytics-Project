e821985a2b4d0dc02ce442d05ee1ee225c44f12d
sharmaneha78 <sharmaneha3720@gmail.com>
2026-01-30 21:21:09 +0530

    Update and rename sql/03_Schema_Design/dim_tables.sql to sql/03_dim_tables.sql

--- sql/03_dim_tables.sql
+++ sql/03_dim_tables.sql
@@ -11,21 +11,25 @@ quarter INT,
 day INT,
 week INT
 )
-SELECT * FROM dim_date;--dim_location
+SELECT * FROM dim_date;
+--dim_location
 CREATE TABLE dim_location (
 location_id INT IDENTITY(1,1) PRIMARY KEY,
 State varchar(100),
 City varchar(100) ,
 Location varchar(200)
-);--dim_restaurant
+);
+--dim_restaurant
 CREATE TABLE dim_restaurant (
 restaurant_id INT IDENTITY(1,1) PRIMARY KEY,
 restaurant_name VARCHAR(200)
-);--DIM_CATEGORY
+);
+--DIM_CATEGORY
 CREATE TABLE dim_category(
 category_id INT IDENTITY(1,1) PRIMARY KEY,
 category VARCHAR(200)
-);--DIM_DISH
+);
+--DIM_DISH
 CREATE TABLE dim_dish(
 dish_id INT IDENTITY(1,1) PRIMARY KEY,
 dish_name VARCHAR(200)
